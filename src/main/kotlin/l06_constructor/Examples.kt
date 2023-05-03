87c8ab7b4b42db06c13c1a016fb0bfc302f933c3
Nurlan Ormond <sunshineforester@gmail.com>
2023-05-03 20:48:36 +0300

    Constructor

---
+++ src/main/kotlin/l06_constructor/Examples.kt
@@ -0,0 +1,21 @@
+package l06_constructor
+
+class InitOrderDemo1(name: String) {
+    val firstProperty = "First property: $name".also(::println)
+
+    init {
+        println("First initializer block that prints $name")
+    }
+
+    val secondProperty = "Second property: ${name.length}".also(::println)
+
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+}
+
+fun main() {
+    val initDemo = InitOrderDemo1("Alex")
+
+    println(initDemo)
+}
