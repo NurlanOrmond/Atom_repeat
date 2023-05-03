dd8dbe65a2d7281bca02e6d843125af5a40e96ba
Nurlan Ormond <sunshineforester@gmail.com>
2023-05-03 16:18:03 +0300

    Can'be combined

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,15 +16,16 @@ fun main(args: Array<String>) {
     val ci: Char = character + int
     val cs: String = character + string
 
-    println("""
-        $si
-        $sd
-        $sb
-        $sc
-        $di
-        $ci
-        $cs
-    """.trimIndent())
+    // Can't be combined:
+
+//  val result9: Int = int + double
+//  val result10: Int = int + boolean
+//  val result11: Int = int + string
+//  val result12: Int = int + character
+//  val result13: Boolean = boolean + int
+//  val result14: Boolean = boolean + string
+//  val result15: Boolean = boolean + double
+//  val result16: Boolean = boolean + character
 
 
 
