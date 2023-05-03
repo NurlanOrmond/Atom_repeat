886d22b3e99d60559fcf9fc34f807af0c3d835df
Nurlan Ormond <sunshineforester@gmail.com>
2023-05-03 16:51:07 +0300

    Something on classes

---
+++ src/main/kotlin/l02expressions/Expressions.kt
@@ -0,0 +1,21 @@
+package l02expressions
+
+// Can't do this:
+// val f = for(i in 1..10) {}
+// Compiler error message:
+// for is not an expression, and
+// only expressions are allowed here
+
+fun main() {
+    val result1 = if (11 > 42) 9 else 5
+
+    val result2 = if (1 < 2) {
+        val a = 11
+        a + 42
+    } else 42
+
+    val result3 =
+        if ('x' < 'y')
+            println("x < y")
+        else
+            println("x > y")
